24dbe17358f48b2e2643663317c141861e437b29
Fredy Álvarez <fre.alvarezb@gmail.com>
2018-12-05 00:51:05 -0500

    Fixing floor texture

--- src/objects/obstacles/floor.hpp
+++ src/objects/obstacles/floor.hpp
@@ -23,7 +23,7 @@ private:
 
 public:
   Floor(float left, float bottom, float depth = 3.0f)
-      : Obstacle(left, bottom, 0, depth) {
+      : Obstacle(left, bottom, 0, 1, 1, depth) {
     setTextureId(texture::load(config::fgSqrTexturePath));
     setRandomTexCoords();
   }
